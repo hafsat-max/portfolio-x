6acbf2debd0226780872b74c74a6bd2ec35aa2cc
hafsat-max <hafsahbusari69@gmail.com>
2023-09-16 15:30:02 +0100

    Completed login

--- api/builder.ts
+++ api/builder.ts
@@ -6,20 +6,20 @@ export const builder = createBuilder({
   api: {
     client: {
       client_list: () => API.get("client_list"),
-      create_client_portfolio: (data) =>
+      create_client_portfolio: (data:any) =>
         API.get("api/client/create_client_portfolio", data),
-      create_industry: (data) => API.get("api/client/create_industry", data),
-      create_organization: (data) =>
+      create_industry: (data:any) => API.get("api/client/create_industry", data),
+      create_organization: (data:any) =>
         API.get("api/client/create_organization", data),
-      industry_list: (data) => API.get("api/client/industry_list", data),
+      industry_list: (data:any) => API.get("api/client/industry_list", data),
       organogram: () => API.get("api/client/organogram"),
     },
     tenant: {
-      create_business: (data) => API.post("api/client/", data),
+      create_business: (data:any) => API.post("api/client/", data),
     },
     login: {
       sign_in: (data: Record<'email'|'password', string> ) => AUTHAPI.post<ILoginResponse>("login/", data),
-      refresh: (data) => AUTHAPI.post("api/client/login", data),
+      refresh: (data:any) => AUTHAPI.post("api/client/login", data),
     },
   },
 });
